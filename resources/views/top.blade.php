aa8d759f4dd1b06aa6d46fa6c364b2456b84eb59
hakotti <yarimojihako@gmail.com>
2019-02-22 13:00:16 +0900

    190222c

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -16,31 +16,12 @@
         <li>{{ __('1. Make new account to <Register> page.') }}<br>
             {{ __('(You might used to 10minutemail)') }}
         </li>
-        <li>{{ __('2. Login to <Login> page.') }}</li>
-        <li>{{ __('All visitors can sign up.') }}</li>
-        <li>{{ __('Each the logged in user can post, edit and delete.') }}</li>
-        <li>
-            {{ __('The admin can edit and delete all users\' accounts and posts.') }}
-            <ul>
-                <li>
-                    {{ __('Admin') }}:
-                    <ul>
-                        <li>id: 1</li>
-                        <li>name: sutara79</li>
-                        <li>email: toumin.m7@gmail.com</li>
-                        <li>password: 1234</li>
-                    </ul>
-                </li>
-                <li>
-                    {{ __('User') }}:
-                    <ul>
-                        <li>id: 2</li>
-                        <li>name: foo1</li>
-                        <li>email: foo1@foo.com</li>
-                        <li>password: 1234</li>
-                    </ul>
-                </li>
-            </ul>
+        <li>{{ __('2. Login to <Login> page.') }}<br>
+            {{ __('(TestAccount is mail:foo1@foo.com pass:password)') }}
+        </li>
+        <li>{{ __('3. Write new Post to <New Post> page.') }}</li>
+        <li>{{ __('4. View Posts to <Posts> page.') }}<br>
+            {{ __('(Push Titlestring, You read detail)') }}
         </li>
     </ul>
     {{-- <ul>
